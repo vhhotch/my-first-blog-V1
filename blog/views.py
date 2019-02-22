f0456e1ddf14c31166bf9f718825030192bd4bbc
vicky <v.h.hotchkiss@gmail.com>
2019-02-22 11:45:18 +0000

    Added views to create/edit blog post inside the site correction of error.

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
-    Post.objects.get(pk=pk)
-def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
